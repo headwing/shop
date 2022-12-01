533ffef758ac18ac67d6f2281aeb4e31bb8ec46c
headwing <110597846+headwing@users.noreply.github.com>
2022-12-02 00:22:18 +0900

    애플코딩 shop 첫 페이지 제작

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import "./App.css";
+import bg from "./img/bg.png";
+import data from "./data.js";
+import { useState } from "react";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div
+        className="mainBg"
+        style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
+
+      <Container>
+        <Row>
+          {shoes.map((shoes, i) => {
+            return <List data={shoes} key={i} />;
+          })}
+        </Row>
+      </Container>
     </div>
+  );
+}
+function List({ data }) {
+  return (
+    <Col sm>
+      <img src={data.img} width="100%" />
+      <h4>{data.title}</h4>
+      <p>{data.content}</p>
+    </Col>
   );
 }
 
